e93d01466d1e9464c9227fd58851369a6625cdb8
Shantanu Kumar <shantanukmr2000@gmail.com>
2023-04-30 13:22:44 +0530

    Added txt file reading

--- FordFulkerson.java
+++ FordFulkerson.java
@@ -1,8 +1,11 @@
 //create a 2d array of pairs graph
 import java.util.*;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 
 public class FordFulkerson {
-    public static void maxFlow(int[][][] graph, int source, int sink) {
+    public static void maxFlow(int[][][] graph, int source, int sink, int V) {
         // Create a copy of the graph to use as the residual graph
         int[][] residualGraph = new int[graph.length][graph[0].length];
         for (int i = 0; i < graph.length; i++) {
@@ -55,16 +58,16 @@ public class FordFulkerson {
         return false; // No augmenting path found
     }
 
-    static void dfs(int[][] rGraph, int s, boolean[] visited) {
+    static void dfs(int[][] rGraph, int s, boolean[] visited, int V) {
         visited[s] = true;
         for (int i = 0; i < V; i++) {
             if (rGraph[s][i] > 0 && !visited[i]) {
-                dfs(rGraph, i, visited);
+                dfs(rGraph, i, visited, V);
             }
         }
     }
 
-    static void findMinCut(int[][][] graph, int s, int t) {
+    static void findMinCut(int[][][] graph, int s, int t, int V) {
       int[][] rGraph = new int[graph.length][graph[0].length];
         for (int i = 0; i < graph.length; i++) {
             for (int j = 0; j < graph[0].length; j++) {
@@ -82,7 +85,7 @@ public class FordFulkerson {
       }
 
       boolean[] visited = new boolean[V];
-      dfs(rGraph, s, visited);
+      dfs(rGraph, s, visited, V);
 
       System.out.println("Minimum st-cut:");
       for (int i = 0; i < V; i++) {
@@ -93,31 +96,61 @@ public class FordFulkerson {
           }
       }
   }
-    static final int V = 8;
+    //static final int V = 8;
     public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number the vertices:");
+        int vertexCount = sc.nextInt();
+        int graph[][][] = new int[vertexCount][vertexCount][2];
         //{flow, capacity}                  s      a       b     c     d       e      f      t
-        int graph[][][] = new int[][][]{{ {0, 0}, {0, 3}, {0, 2}, {0, 0}, {0, 5}, {0, 0}, {0, 0}, {0, 0} }, 
-                                        { {0,0}, {0,0}, {0,0}, {0,0}, {0,0}, {0,2}, {0,0}, {0,4} },
-                                        { {0,0}, {0,0}, {0,0}, {0,0}, {0,0}, {0,0}, {0, 4}, {0,0} },  
-                                        { {0,0}, {0,5}, {0,0}, {0,0}, {0,0}, {0,0}, {0,0}, {0,0} },
-                                        { {0,0}, {0,0}, {0,3}, {0,4}, {0,0}, {0,0}, {0,0}, {0,0} },   
-                                        { {0,0}, {0,0}, {0,0}, {0,2}, {0,0}, {0,0}, {0,0}, {0,3} },
-                                        { {0,0}, {0,0}, {0,0}, {0,2}, {0,0}, {0,0}, {0,0}, {0,3}  },
-                                        { {0,0}, {0,0}, {0,0}, {0,0}, {0,0}, {0,0}, {0,0}, {0,0}  }
-        };
+        // int graph[][][] = new int[][][]{{ {0, 0}, {0, 3}, {0, 2}, {0, 0}, {0, 5}, {0, 0}, {0, 0}, {0, 0} }, 
+        //                                 { {0,0}, {0,0}, {0,0}, {0,0}, {0,0}, {0,2}, {0,0}, {0,4} },
+        //                                 { {0,0}, {0,0}, {0,0}, {0,0}, {0,0}, {0,0}, {0, 4}, {0,0} },  
+        //                                 { {0,0}, {0,5}, {0,0}, {0,0}, {0,0}, {0,0}, {0,0}, {0,0} },
+        //                                 { {0,0}, {0,0}, {0,3}, {0,4}, {0,0}, {0,0}, {0,0}, {0,0} },   
+        //                                 { {0,0}, {0,0}, {0,0}, {0,2}, {0,0}, {0,0}, {0,0}, {0,3} },
+        //                                 { {0,0}, {0,0}, {0,0}, {0,2}, {0,0}, {0,0}, {0,0}, {0,3}  },
+        //                                 { {0,0}, {0,0}, {0,0}, {0,0}, {0,0}, {0,0}, {0,0}, {0,0}  }
+        // };
+
+        for(int i = 0; i < vertexCount; i++){
+            for(int j = 0; j < vertexCount; j++){
+                graph[i][j][0] = 0;
+                graph[i][j][1] = 0;
+            }
+        }
+        //filename.txt input format is "vertex1 vertex2 capacity"
+        try {
+            File file = new File("filename1.txt");
+            Scanner scanner = new Scanner(file);
+      
+            while (scanner.hasNextLine()) {
+                String line = scanner.nextLine();
+                String[] line1 = line.split(" ");
+                int i = Integer.parseInt(line1[0]); //vertex1
+                int j = Integer.parseInt(line1[1]); //vertex2   
+                int k = Integer.parseInt(line1[2]); //capacity
+                graph[i][j][1] = k;
+            }
+      
+            scanner.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("File not found.");
+        }
+      
 
         //specify the source and the sink
-        int source = 0, sink = 7;
+        int source = 0, sink = vertexCount-1;
         int max_flow = 0;
         //int maxflow = maxFlow(graph, source, sink);
-        maxFlow(graph, source, sink);
+        maxFlow(graph, source, sink, vertexCount);
         //System.out.println(maxflow);
-        for(int i = 0; i < 8; i++){
+        for(int i = 0; i < vertexCount; i++){
             max_flow += graph[0][i][0];
             //System.out.println(graph[0][i][0]);
         }
         System.out.println("Maxflow is: " + max_flow);
-        findMinCut(graph, 0, 7);
+        findMinCut(graph, source, sink, vertexCount);
     }
 
 }
